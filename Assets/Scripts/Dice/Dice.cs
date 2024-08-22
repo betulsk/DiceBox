bb8ebdd30804c502363e410323a985fa553439b6
Betul <betul.s.99@hotmail.com>
2024-08-22 21:00:22 +0300

    Dice fixed

--- Assets/Scripts/Dice/Dice.cs
+++ Assets/Scripts/Dice/Dice.cs
@@ -13,9 +13,12 @@ public struct DiceDatas
 
 public class Dice : MonoBehaviour
 {
-    [SerializeField] private Rigidbody _rigidbody;
-    [SerializeField] private float _throwForce;
+    [SerializeField] private DiceAnimationController _diceAnimationController;
     [SerializeField] private List<Transform> _diceFaces;
+    [SerializeField] private Rigidbody _rigidbody;
+
+    [SerializeField] private float _throwForce;
+    [SerializeField] private float _rollForce;
 
     private void Start()
     {
@@ -31,7 +34,7 @@ public class Dice : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.K))
         {
-            GetNumberOnTop();
+            RollDice();
         }
     }
 
@@ -42,7 +45,7 @@ public class Dice : MonoBehaviour
 
     public void GetNumberOnTop()
     {
-        if (_diceFaces == null)
+        if(_diceFaces == null)
         {
             return;
         }
@@ -61,11 +64,27 @@ public class Dice : MonoBehaviour
 
     private void RollDice()
     {
-        //_rigidbody.AddForce(transform.forward * (_throwForce + UnityEngine.Random.Range(-1f, 1f)), ForceMode.Impulse);
+        Debug.Log("!RollDice");
+        var randomVariance = UnityEngine.Random.Range(-1f, 1f);
+        _rigidbody.AddForce(-_rigidbody.gameObject.transform.up * (_throwForce + randomVariance), ForceMode.Impulse);
+        var randX = UnityEngine.Random.Range(0f, 1f);
+        var randY = UnityEngine.Random.Range(0f, 1f);
+        var randZ = UnityEngine.Random.Range(0f, 1f);
+        _rigidbody.AddTorque(new Vector3(randX, randY, randZ) * (_rollForce + randomVariance), ForceMode.Impulse);
+        Debug.Log("!Next step is anim");
 
+        StartCoroutine(this.WaitForSeconds(1f, () =>
+        {
+            Debug.Log("!Anim must play");
+            StopRolling();
+            _diceAnimationController.Animator.enabled = true;
+            _diceAnimationController.PlayAnim(EAnim.Dice_2);
+        }));
     }
 
     public void StopRolling()
     {
+        _rigidbody.velocity = Vector3.zero;
+        _rigidbody.angularVelocity = Vector3.zero;
     }
 }
--- Assets/Scripts/Dice/DiceSpawnController.cs
+++ Assets/Scripts/Dice/DiceSpawnController.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class DiceSpawnController : MonoBehaviour
 {
     private int _diceCount = 2;
-    [SerializeField] private GameObject _dicePrefab;
+    [SerializeField] private Dice _dicePrefab;
     [SerializeField] private List<Transform> _spawnPositions;
 
     private void Start()
@@ -29,15 +29,8 @@ public class DiceSpawnController : MonoBehaviour
         for(int i = 0; i < 1; i++)
         {
             int random = UnityEngine.Random.Range(0, _spawnPositions.Count);
-            GameObject diceObject = Instantiate(_dicePrefab, _spawnPositions[random].position, Quaternion.identity);
-            Dice dice = diceObject.GetComponentInChildren<Dice>();
-            dice.Rb.useGravity = true;
-            dice.Rb.isKinematic = false;
-            dice.AddForceRb(_spawnPositions[random].position);
-            dice.GetNumberOnTop();
-            dice.RollDice(GameManager.Instance.DiceDatas.FirstData);
-            dice.Rb.useGravity = false;
-            dice.Rb.isKinematic = true;
+            Dice diceObject = Instantiate(_dicePrefab, _spawnPositions[random].position, Quaternion.identity, transform);
+            
         }
     }
 }
