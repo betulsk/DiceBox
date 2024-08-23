b1fe47377b2a0deb9920777e60bd457bf86ff43f
Betul <betul.s.99@hotmail.com>
2024-08-24 00:53:17 +0300

    Punch animation added

--- Assets/Scripts/UI/InventoryWidget/InventoryGroup.cs
+++ Assets/Scripts/UI/InventoryWidget/InventoryGroup.cs
@@ -1,17 +1,26 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class InventoryGroup : MonoBehaviour
 {
+    private Transform _initTransform;
+    private Coroutine _punchRoutine;
     [SerializeField] private EItemType _itemType;
     [SerializeField] private Image _inventoryIconImage;
     [SerializeField] private TMP_Text _inventoryAmountText;
+    [SerializeField] private Animator _animator;
 
     public EItemType ItemType
     {
         get { return _itemType; }
         set { _itemType = value; }
+    }
+
+    private void OnEnable()
+    {
+        _initTransform = transform;
     }
 
     public void SetGroupValues(EItemType itemType, int amount)
@@ -24,5 +33,11 @@ public class InventoryGroup : MonoBehaviour
     public void SetInventoryAmount(int amount)
     {
         _inventoryAmountText.text = amount.ToString();
+        _animator.enabled = true;
+        StartCoroutine(this.WaitForSeconds(.4f, () =>
+        {
+            _animator.enabled = false;
+            transform.localScale = Vector3.one;
+        }));
     }
 }
